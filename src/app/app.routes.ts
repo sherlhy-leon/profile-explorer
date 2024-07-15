ddabea0e26efac34f41bbff4b53289ff2b1616af
Sherlhy Leon Lopez <sherlhy95@gmail.com>
2024-07-15 17:11:11 +0200

    Add error page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { UserProfileDetailsComponent } from './pages/user-profile-details/user-profile-details.component';
 import { ProfileExplorerComponent } from './pages/profile-explorer/profile-explorer.component';
+import { ErrorComponent } from './pages/error/error.component';
 
 export const routes: Routes = [
     { path: '', component: ProfileExplorerComponent },
-    { path: 'user/:userlogin', component: UserProfileDetailsComponent }
+    { path: 'user/:userlogin', component: UserProfileDetailsComponent },
+    { path: 'error', component: ErrorComponent }
 ];
--- src/app/pages/error/error.component.ts
+++ src/app/pages/error/error.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-error',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, HeaderComponent],
   templateUrl: './error.component.html',
   styleUrl: './error.component.scss'
 })
